d31045355ebe2a7a633a6964da119de5d2fe8c09
DEevid20 <defernandodelatorre06@gmail.com>
2026-03-16 18:20:30 -0500

    primer commit ejercicio herencia

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main  main {
+    public  static  void main (String[] args){
+
+
+        empleado e1 = new empleadoadministrativosss();
+        empleadoadministrativosss ea1=new empleadoadministrativosss("e123456" , "pedro")
+        empleado ev1 = new empleadoadministrativosss();
+
+        empleado eh1 = new empleadoadministrativosss();
+
+
+
+    }
+
+}
---
+++ src/empleadoadministrativosss.java
@@ -0,0 +1,32 @@
+public class empleadoadministrativosss extends empleado{
+
+    private double bonificacionFija;
+
+    public empleadoadministrativosss() {
+    }
+
+    public empleadoadministrativosss(String identificacion, String nombre, int edad, Double salario, double bonificacionFija) {
+        super(identificacion, nombre, edad, salario);
+        this.bonificacionFija = bonificacionFija;
+    }
+
+    public double getBonificacionFija() {
+        return bonificacionFija;
+    }
+
+    public void setBonificacionFija(double bonificacionFija) {
+        this.bonificacionFija = bonificacionFija;
+    }
+
+    public void mostrarInf(){
+        System.out.println("identificanio" +identificacion);
+        System.out.println("nombre" + nombre);
+        System.out.println("salario" +salario);
+        System.out.println("edad" +edad);
+        System.out.println("bonificacion" +bonificacionFija);
+
+    }
+
+
+
+}
